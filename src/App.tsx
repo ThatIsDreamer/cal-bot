775c27684ec7d766cc9e731a0a245fa252a8a0fc
Dreamer <n_grisha@inbox.ru>
2025-06-26 23:26:00 +0300

    Layout hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -253,7 +253,7 @@ function App() {
               justifyContent: 'center',
               alignItems: 'center'
             }}>
-              <div className='w-20'>
+              <div className='w-50'>
                 <WheelPickerWrapper className='border-0'>
                   <WheelPicker
                     options={heightOptions}
@@ -297,7 +297,7 @@ function App() {
               alignItems: 'center'
             }}>   
               
-              <div className="w-20">
+              <div className="w-50">
               <WheelPickerWrapper className='border-0'>
                     <WheelPicker
                       options={weightOptions}
