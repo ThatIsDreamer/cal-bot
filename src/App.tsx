b255153e50183c91bd06b898e81f135d07decadd
ThatIsDreamer <n_grisha@inbox.ru>
2025-07-01 07:55:48 +0800

    haptic fix 3.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { mockTelegramEnv, emitEvent} from '@telegram-apps/bridge';
 //import { useRawInitData } from '@telegram-apps/sdk-react';
 import { useLaunchParams } from '@telegram-apps/sdk-react';
-import { hapticFeedbackSelectionChanged } from '@telegram-apps/sdk';
+import { hapticFeedbackSelectionChanged, init } from '@telegram-apps/sdk';
 import myAnimation from './assets/wave.gif';
 import maleAnimation from './assets/male.gif';
 import femaleAnimation from './assets/female.gif';
@@ -81,6 +81,7 @@ type Gender = 'male' | 'female' | 'secret';
 
 function App() {  
   eruda.init();
+  init();
   //const initDataRaw = useRawInitData()
   const launchParams = useLaunchParams()
   
